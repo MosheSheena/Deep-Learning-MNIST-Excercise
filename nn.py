4fa49f9df1822997c3cb8ecf1c89ebbafd8d612b
moshesheena <moshesheena@gmail.com>
2018-06-05 17:35:17 +0300

    Almost finished with logistic regression - requires minor adjustments

--- nn.py
+++ nn.py
@@ -6,20 +6,25 @@ image_pixels = 28 ** 2
 num_classes = 10
 
 
-def inference(x):
+def inference(
+        x, w_initializer=tf.random_uniform_initializer,
+        bias_initializer=tf.ones_initializer
+):
     """
     produces a probabilty distribution over the output classes
     given a minibatch
     :param x: (matrix) the minibatch
-    :return:  (tf.nn.softmax) returns softmax output
+    :param w_initializer:
+    :param bias_initializer:
+    :return: softmax output
     """
     tf.constant_initializer(value=0)
     W = tf.get_variable(
         "W", shape=[image_pixels, num_classes],
-        initializer=tf.random_uniform_initializer
+        initializer=w_initializer
     )
     b = tf.get_variable(
-        "b", shape=[num_classes], initializer=tf.ones_initializer
+        "b", shape=[num_classes], initializer=bias_initializer
     )
     output = tf.nn.softmax(tf.matmul(x, W) + b)
     return output
@@ -27,7 +32,7 @@ def inference(x):
 
 def loss(output, y):
     """
-    compute the average error per data sample
+    compute the average error per data sample using the Cross-Entropy function
     :param output: the inference of x
     :param y: data labels
     :return: loss over data
@@ -39,33 +44,59 @@ def loss(output, y):
     return loss_val
 
 
-def training(cost, global_step, learning_rate=None):
+def training(loss, global_step, learning_rate=None, logging=False):
     """
     train the model by computing the gradients and modifying the model params
-    :param cost: (tensor) the value given from the loss function
+    :param loss: (tensor) the value given from the loss function
     :param global_step:
     :return: accuracy
     """
+    if logging:
+        tf.summary.scalar("loss", loss)
     optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
-    train_op = optimizer.minimize(cost, global_step=global_step)
+    train_op = optimizer.minimize(loss, global_step=global_step)
 
     return train_op
 
 
 def evaluate(output, y):
     """
-
+    evaluating the model on the validation / test-set
     :param output:
     :param y:
     :return:
     """
-    correct_prediction = tf.equal(
-        tf.arg_max(output, 1), tf.argmax(y, 1)
-    )
-    accuracy = tf.reduce_mean(
-        tf.cast(correct_prediction, tf.float32)
-    )
+    correct_prediction = tf.equal(tf.arg_max(output, 1), tf.argmax(y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
     return accuracy
 
 
+def logistic():
+
+    with tf.Graph().as_default():
+
+        x = tf.placeholder("float", [None, image_pixels])
+
+        y = tf.placeholder("float", [None, num_classes])
+
+        output = inference(x)
+        cost = loss(output=output, y=y)
+        global_step = tf.Variable(0, name='global step', trainable=False)
+
+        train_op = training(cost, global_step=global_step)
+        eval_op = evaluate(output=output, y=y)
+        summary_op = tf.summary.merge_all()
+        saver = tf.train.Saver()
+
+        sess = tf.Session()
+        summary_writer = tf.summary.FileWriter\
+            (
+                "/Users/moshesheena/git/Deep-Learning-MNIST-Excercise/logs",
+                graph_def=sess.graph_def
+            )
+        init_op = tf.initialize_all_variables()
+
+        sess.run(init_op)
+
+        
